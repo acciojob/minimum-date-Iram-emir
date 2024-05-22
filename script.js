c8b6f83d89f66fd298b5d0ee639d52a78f8b4e88
iram-emir <158573737+Iram-emir@users.noreply.github.com>
2024-05-23 00:24:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function minDate(dates) {
   //write you code here
+	
+    const dateObjects = dates.map(dateStr => new Date(dateStr.replace(/\//g, '-')));
+
+    // Find the minimum date object
+    const minDateObj = new Date(Math.min(...dateObjects));
+
+    // Format the minimum date object back to the "YYYY/MM/DD" format
+    const year = minDateObj.getFullYear();
+    const month = String(minDateObj.getMonth() + 1).padStart(2, '0');
+    const day = String(minDateObj.getDate()).padStart(2, '0');
+
+    return `${year}/${month}/${day}`;
+}
+
+// Test cases
+console.log(minDate(["2023/03/01", "2023/03/02", "2023/03/03"])); // "2023/03/01"
+console.log(minDate(["2023/01/01", "2023/02/02", "2022/12/31"])); // "2022/12/31"
+
 }
 
 // Do not change the code
